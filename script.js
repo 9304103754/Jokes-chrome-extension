129dc702abe53e505327f490ee8313078c41880d
Ravi Kumar jha <jravi40800@gmaile.com>
2024-09-24 22:35:52 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+fetch('https://icanhazdadjoke.com/', {
+    headers: {
+        'Accept': 'application/json'
+    }
+})
+.then(response => response.json())
+.then(jokeData => {
+    const jokeText = jokeData.joke; // Correct property name
+    const jokeElement = document.getElementById('jokeElement');
+    jokeElement.innerHTML = jokeText;
+})
+.catch(error => {
+    console.error('Error fetching joke:', error);
+});
